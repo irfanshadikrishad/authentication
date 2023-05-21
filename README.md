9835c9952b5dfdc31746b72a496a59e93e001c09
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-05-21 17:30:33 +0600

    Level 5: Cookies & Sessions with Passport.js

---
+++ README.md
@@ -0,0 +1,11 @@
+# authentication
+
+It's a repository to visulize and learn about different levels of database authentication. To use it you need [NodeJS](https://nodejs.org/en) and [MongoDB](https://www.mongodb.com/try/download/community) installed in your local machine. And to install the dependensies run `npm install` in CLI. <br> (The database is not connected to the cloud rather in the localhost)
+
+### To go back to the previous versions or levels, checkout to the commited SHA —
+
+- to go back to "Level 1: Username & Password" version of the code —
+  `git checkout c1518e2ee9258ef933137978b643a37846157c9f .`
+- to go back to "Level 2: Encryption" version of the code —
+  `git checkout 7dd8e78af573cfd178742022e52b5e62a76c591a .`
+- and so on ...
--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: process.env.SECRET, // any long string
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize()); // initialize passport
+app.use(passport.session()); // use passport to manage session
 
 mongoose.connect("mongodb://127.0.0.1:27017/secrets");
-const Schema = mongoose.Schema({
+const Schema = new mongoose.Schema({
   email: String,
   password: String,
 });
-const User = mongoose.model("users", Schema);
+Schema.plugin(passportLocalMongoose); //plugin added to schema
+const User = new mongoose.model("users", Schema);
+
+passport.use(User.createStrategy()); // local login staratigies
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -27,51 +42,60 @@ app
     res.render("login");
   })
   .post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username })
-      .then((data) => {
-        bcrypt.compare(password, data.password, (err, result) => {
-          if (result == true) {
-            console.log(`logged in as —${username}`);
-            res.render("secrets");
-          } else {
-            res.send("—wrong username or password");
-          }
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+          console.log(`logged in as —${req.body.username}`);
         });
-      })
-      .catch((err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
+      }
+    });
   });
+app.route("/secrets").get((req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 app
   .route("/register")
   .get((req, res) => {
     res.render("register");
   })
   .post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-      const newUser = new User({
-        email: username,
-        password: hash,
-      });
-      newUser
-        .save()
-        .then(() => {
-          console.log(`${username} —registered`);
-          res.render("secrets");
-        })
-        .catch((err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
-    });
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
+        } else {
+          passport.authenticate("local")(req, res, () => {
+            res.redirect("/secrets");
+            console.log(`${req.body.username} —registered`);
+          });
+        }
+      }
+    );
   });
+
+app.route("/logout").get((req, res) => {
+  req.logOut((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 app.listen(5000, () => {
   console.log("listening to —5000");
